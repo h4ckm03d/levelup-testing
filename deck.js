9f2a96c081e0e45eaae3a9227bf044cf179c90c1
Moch Lutfi <moch.lutfi@bukalapak.com>
2019-07-26 06:03:18 +0700

    update slide using code explanation

---
+++ deck.js
@@ -0,0 +1,15 @@
+export { draculaFull as theme } from 'code-surfer'
+
+import { slides as introduction } from './introduction.mdx'
+import { slides as mistakes } from './common-mistakes.mdx'
+import { slides as design } from './design.mdx'
+import { slides as tips } from './tips.mdx'
+import { slides as recap } from './recap.mdx'
+
+export const slides = [
+    ...introduction,
+    ...mistakes,
+    ...design,
+    ...tips,
+    ...recap,
+]
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/h4ckm03d/levelup-testing
+
+go 1.12
+
+require github.com/google/go-cmp v0.3.0
